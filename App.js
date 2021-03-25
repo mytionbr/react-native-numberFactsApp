eb13a1e060bb5866479330ade85acf984227744b
mytionBr <matheuss.jesus2017@gmail.com>
2021-03-25 02:34:46 -0300

    atualizando o projeto

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react'
+import NumberFact from './scr/screen/NumberFact'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NumberFact/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ scr/screen/NumberFact.js
@@ -0,0 +1,86 @@
+import React,{useState,useEffect} from 'react'
+import { StyleSheet,  View } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Text,Input,Item,Body,Title,Left,Right } from 'native-base';
+
+const NumberFact = () => {
+    
+    const initialFact = {
+        text: ''
+    }
+    
+    const [fact,setFact] = useState(initialFact)
+
+    const handleInput = (value)=>{
+        value = value.replace(/\s/g, '')
+        handleIpi(value)
+    }
+
+    const handleIpi = (number)=>{
+        if (number != '') {
+            fetch(`http://numbersapi.com/${number}`)
+                .then(response => response.text())
+                .then( data =>{   
+                    setFact({text:data})
+                })
+                .catch(err => console.log(err))
+        }else{
+            setFact({text:''})
+        }
+    }
+    
+
+    
+  return (
+    <Container style={{backgroundColor:'#76D7C4'}}>
+    <Header style={{backgroundColor:'#0E6655'}}>
+    <Left style={{flex:1}}/>
+          <Body style={{flex:3  }}>
+            <Title style={{alignSelf:'center'}}>Fato numérico</Title>
+          </Body>
+          <Right style={{flex:1}} />
+    </Header>
+    <Content>
+      <Card >
+        <CardItem header style={{backgroundColor:'#1ABC9C'}}>
+          <Text style={{color:'#fff',textAlign:'center'}}>Insira um número e obtenha um fato aleatório</Text>
+        </CardItem>
+        <CardItem style={{backgroundColor:'#1ABC9C'}}>
+          <Body>
+          <Item regular style={{backgroundColor:'#fff'}}>
+          <Input 
+                keyboardType='numeric' 
+                placeholder='Ex: 2000' 
+                onChangeText={(value)=> handleInput(value)} />
+          </Item>
+          </Body>
+        </CardItem>
+        <CardItem footer style={{backgroundColor:'#1ABC9C'}}>
+          <Text style={{color:'#fff',textAlign:'center'}}>{fact.text}</Text>
+        </CardItem>
+     </Card>
+    </Content>
+  </Container>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainContainer:{
+
+  },
+  curiosity:{
+
+  },
+  descContainer:{
+
+  }
+});
+
+export default NumberFact
